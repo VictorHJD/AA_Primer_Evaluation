fb3701dc09eb38f547fd4d31730a615b0fcf7604
Victor Jarquin <victor@harriet.biologie.hu-berlin.de>
2019-11-15 15:08:25 +0100

    Estimation of TM and GC from primers in a separated code

---
+++ R/General_Primer_information.R
@@ -0,0 +1,43 @@
+###Information from all the primer pairs evaluated 
+library("TmCalculator")
+
+####Primer information
+primerInput <- read.csv("/localstorage/victor/AA_Primer_evaluation/primerInputUnique.csv") 
+primerInput$Primer_name <- gsub(pattern = " ", replacement = "", x = primerInput$Primer_name)
+primerInput$X <- NULL
+primerInput$Primer_name <- gsub(pattern = " ", replacement = "", x = primerInput$Primer_name)
+
+##Primer characteristics (Tm and GC)
+primerInput$Seq_F <- as.vector(primerInput$Seq_F)
+primerInput$Seq_R <- as.vector(primerInput$Seq_R)
+
+primerInput <- primerInput[-c(20,21,82),] ###Eliminating primers with inosine ... To make the loop work :S 
+
+output <- data.frame(matrix(nrow = nrow(primerInput), ncol = 5))
+colnames(output) <- c("Primer_name","Tm_F", "Tm_R", "GC_F", "GC_R")
+output[,1]<-primerInput$Primer_name
+
+for (row in 1: nrow(primerInput))
+{
+  Fwd <- primerInput[row, "Seq_F"]
+  Rev <- primerInput[row, "Seq_R"]
+  
+  TmF <- Tm_NN(Fwd, ambiguous = T, imm_table = "DNA_IMM1")
+  TmR <- Tm_NN(Rev, ambiguous = T, imm_table = "DNA_IMM1")
+  GCF <- GC(Fwd, ambiguous = T)
+  GCR <- GC(Rev, ambiguous = T)
+  
+  output[row, 2] <- TmF
+  output[row, 3] <- TmR
+  output[row, 4] <- GCF
+  output[row, 5] <- GCR
+}
+
+primerInput <- read.csv("~/AA_Primer_evaluation/primerInputUnique.csv")
+primerInput$Primer_name <- gsub(pattern = " ", replacement = "", x = primerInput$Primer_name)
+primerInput <- merge(primerInput, output, by= "Primer_name", all= T)
+primerInput$X <- NULL
+primerInput$Primer_name <- gsub(pattern = " ", replacement = "", x = primerInput$Primer_name)
+
+rm(GCF, GCR, Fwd, Rev, row, TmF, TmR)
+#write.csv(primerInput, "~/AA_Primer_evaluation/primerOutputUnique.csv")
--- R/Primer_analysis.R
+++ R/Primer_analysis.R
@@ -7,7 +7,6 @@ library(vegan)
 library(gridExtra)
 library(grid)
 library(lattice)
-library("TmCalculator")
 #library("ranacapa")
 library("pheatmap")
 library("viridisLite")
@@ -150,57 +149,8 @@ rawcounts %>%
   rowwise() %>%
   mutate(Total_reads = sum(Raw_counts_Fox, Raw_counts_Racc)) -> rawcounts
 
-rm(rawcountsF)
-rm(rawcountsR)
-rm(along)
+rm(rawcountsF, rawcountsR, along)
  
-####Primer information
-primerInput <- read.csv("/localstorage/victor/AA_Primer_evaluation/primerInputUnique.csv") 
-primerInput$Primer_name <- gsub(pattern = " ", replacement = "", x = primerInput$Primer_name)
-primerInput$X <- NULL
-primerInput$Primer_name <- gsub(pattern = " ", replacement = "", x = primerInput$Primer_name)
-##Primer characteristics (Tm and GC)
-primerInput$Seq_F <- as.vector(primerInput$Seq_F)
-primerInput$Seq_R <- as.vector(primerInput$Seq_R)
-
-primerInput <- primerInput[-c(20,21,82),] ###Eliminating primers with inosine ... To make the loop work :S 
-
-output <- data.frame(matrix(nrow = nrow(primerInput), ncol = 5))
-colnames(output) <- c("Primer_name","Tm_F", "Tm_R", "GC_F", "GC_R")
-output[,1]<-primerInput$Primer_name
-
-for (row in 1: nrow(primerInput))
-{
-  Fwd <- primerInput[row, "Seq_F"]
-  Rev <- primerInput[row, "Seq_R"]
-  
-  TmF <- Tm_NN(Fwd, ambiguous = T, imm_table = "DNA_IMM1")
-  TmR <- Tm_NN(Rev, ambiguous = T, imm_table = "DNA_IMM1")
-  GCF <- GC(Fwd, ambiguous = T)
-  GCR <- GC(Rev, ambiguous = T)
-  
-  output[row, 2] <- TmF
-  output[row, 3] <- TmR
-  output[row, 4] <- GCF
-  output[row, 5] <- GCR
-  
-}
-
-primerInput <- read.csv("~/AA_Primer_evaluation/primerInputUnique.csv")
-primerInput$Primer_name <- gsub(pattern = " ", replacement = "", x = primerInput$Primer_name)
-primerInput <- merge(primerInput, output, by= "Primer_name", all= T)
-primerInput$X <- NULL
-primerInput$Primer_name <- gsub(pattern = " ", replacement = "", x = primerInput$Primer_name)
-
-rm(GCF)
-rm(GCR)
-rm(Fwd)
-rm(Rev)
-rm(row)
-rm(TmF)
-rm(TmR)
-#write.csv(primerInput, "~/AA_Primer_evaluation/primerOutputUnique.csv")
-
 #n16S <- subset(primerInput, primerInput$Gen== "16S")
 #n18S <- subset(primerInput, primerInput$Gen== "18S")
 #n28S <- subset(primerInput, primerInput$Gen== "28S")
@@ -997,9 +947,9 @@ ggplot(AbPhy, aes(x = Expected, y = Total_reads), geom=c("point", "smooth")) +
   guides(colour = guide_legend(override.aes = list(size=10))) +
   #geom_hline(yintercept=10000, linetype="dashed", color = "black") + 
   theme(text = element_text(size=20))+
-  labs(tag = "A)")+
-  stat_cor(label.x = 500, label.y = log10(3000000), aes(label= paste(..rr.label.., ..p.label.., sep= "~`,`~")))+
-  stat_regline_equation(label.x = 500, label.y = log10(2200000))
+  labs(tag = "A)")#+
+  #stat_cor(label.x = 500, label.y = log10(3000000), aes(label= paste(..rr.label.., ..p.label.., sep= "~`,`~")))+
+  #stat_regline_equation(label.x = 500, label.y = log10(2200000))
 
 ggplot(AbPhy, aes(x = Expected, y = Total_reads, color= Gen), geom=c("point", "smooth")) +
      scale_x_continuous(name = "Expected amplicon size") +
