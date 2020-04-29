0d51fa28eff5e550616d7ba63287f990543d3a2f
Victor Jarquin <victor@harriet.biologie.hu-berlin.de>
2020-04-29 17:11:30 +0200

    Change code to retrieve 18S taxonomy

--- R/Taxonomy_Database.R
+++ R/Taxonomy_Database.R
@@ -24,32 +24,30 @@ env2list <- function(env){
 source("~/GitProjects/AA_Primer_Evaluation/R/In_silico_primer_evaluation.R")
 
 ##Load data base 18S 
-Seq_18S_db<- readDNAStringSet("/SAN/db/blastdb/18S_ENA/18S_All_ENA.fasta.gzcleaned.fasta", format = "fasta")
+Seq_18S_db<- Biostrings::readDNAStringSet("/SAN/db/ENA_marker/18S_All_ENA.fasta.gz1700cleaned.fasta", format = "fasta")
+#Seq_18S_db<- Biostrings::readDNAStringSet("/SAN/db/blastdb/18S_ENA/Full1700_18S.fasta", format = "fasta") ##Missing characters in the GenBank accession number
+
 ##Align seqs
 #AlignSeqs(Seq_18S_db)
 
 ##Extract names from the sequences 
 Seq_18S_names<- Seq_18S_db@ranges@NAMES
-accession_18S <- as.data.frame(str_extract(Seq_18S_names, "KY\\d+.1")) ##Extract accession numbers from them 
+accession_18S <- as.data.frame(str_extract(Seq_18S_names, "[:alpha:][:alpha:]\\d+.1")) ##Extract accession numbers from them 
 colnames(accession_18S)<- "Accesion_number"
 
 ##convert accession numbers to taxonomic IDs 
 ##SQLite database is already in Harriet
-taxID_18S<- accessionToTaxa(as.character(accession_18S$Accesion_number), sqlFile = "/SAN/db/taxonomy/taxonomizr.sql")
+taxID_18S<- accessionToTaxa(as.character(accession_18S$Accesion_number), sqlFile = "/SAN/db/taxonomy/taxonomizr.sql") ##Not working :S
 accession_18S$Taxonomic_ID<- taxID_18S
 rm(taxID_18S)
 
 ##convert taxonomic IDs to taxonomy
-taxonomy_18S_DB<- getTaxonomy(as.character(accession_18S$Taxonomic_ID), sqlFile = "/SAN/db/taxonomy/taxonomizr.sql")
+Taxonomy_18S<- taxonomizr::getTaxonomy(as.character(accession_18S$Taxonomic_ID), sqlFile = "/SAN/db/taxonomy/taxonomizr.sql")
 
-temporal<- as.data.frame(taxonomy_18S_DB)
-rm(taxonomy_18S_DB)
-rownames(temporal) <- c(1:nrow(temporal))
-temporal$Taxonomic_ID<- accession_18S$Taxonomic_ID
-temporal$Accession_18S<- accession_18S$Accesion_number
-
-Taxonomy_18S<- temporal
-rm(temporal)
+Taxonomy_18S<- as.data.frame(Taxonomy_18S)
+rownames(Taxonomy_18S) <- c(1:nrow(Taxonomy_18S))
+Taxonomy_18S$Taxonomic_ID<- accession_18S$Taxonomic_ID
+Taxonomy_18S$Accession_18S<- accession_18S$Accesion_number
 
 ##Get counts of unique taxa per taxonomic level
 Unique_taxa<- as.data.frame(sapply(Taxonomy_18S, function(x) n_distinct(x, na.rm = T)))
